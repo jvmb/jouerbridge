96ae40a277d8df09904016c522b72e5883374ffc
marcbeaulieu <marcbeaulieu123@gmail.com>
2016-09-04 08:24:16 -0400

    Commit la version

--- src/main/java/com/jvmb/jouerbridge/model/PlanchetteBridge.java
+++ src/main/java/com/jvmb/jouerbridge/model/PlanchetteBridge.java
@@ -1,5 +1,6 @@
 package com.jvmb.jouerbridge.model;
 
+import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -13,164 +14,79 @@ import java.util.List;
  */
 public class PlanchetteBridge {
 
-    // main sud
-    private List<String> sudPiques = new LinkedList<String>();
-    private List<String> sudCoeurs = new LinkedList<String>();
-    private List<String> sudCarreaux = new LinkedList<String>();
-    private List<String> sudTrefles = new LinkedList<String>();
+    private List<String> mainSud = new LinkedList<String>();
+    private List<String> mainOuest = new LinkedList<String>();
+    private List<String> mainNord = new LinkedList<String>();
+    private List<String> mainEst = new LinkedList<String>();
 
-    // main ouest
-    private List<String> ouestPiques = new LinkedList<String>();
-    private List<String> ouestCoeurs = new LinkedList<String>();
-    private List<String> ouestCarreaux = new LinkedList<String>();
-    private List<String> ouestTrefles = new LinkedList<String>();
-
-    // main nord
-    private List<String> nordPiques = new LinkedList<String>();
-    private List<String> nordCoeurs = new LinkedList<String>();
-    private List<String> nordCarreaux = new LinkedList<String>();
-    private List<String> nordTrefles = new LinkedList<String>();
-
-    // main est
-    private List<String> estPiques = new LinkedList<String>();
-    private List<String> estCoeurs = new LinkedList<String>();
-    private List<String> estCarreaux = new LinkedList<String>();
-    private List<String> estTrefles = new LinkedList<String>();
-
-    public List<String> getSudPiques() {
-        return sudPiques;
+    public List<String> getMainSud() {
+        return mainSud;
     }
 
-    public void setSudPiques(List<String> sudPiques) {
-        this.sudPiques = sudPiques;
+    public void setMainSud(List<String> mainSud) {
+        this.mainSud = mainSud;
     }
 
-    public List<String> getSudCoeurs() {
-        return sudCoeurs;
+    public List<String> getMainOuest() {
+        return mainOuest;
     }
 
-    public void setSudCoeurs(List<String> sudCoeurs) {
-        this.sudCoeurs = sudCoeurs;
+    public void setMainOuest(List<String> mainOuest) {
+        this.mainOuest = mainOuest;
     }
 
-    public List<String> getSudCarreaux() {
-        return sudCarreaux;
+    public List<String> getMainNord() {
+        return mainNord;
     }
 
-    public void setSudCarreaux(List<String> sudCarreaux) {
-        this.sudCarreaux = sudCarreaux;
+    public void setMainNord(List<String> mainNord) {
+        this.mainNord = mainNord;
     }
 
-    public List<String> getSudTrefles() {
-        return sudTrefles;
+    public List<String> getMainEst() {
+        return mainEst;
     }
 
-    public void setSudTrefles(List<String> sudTrefles) {
-        this.sudTrefles = sudTrefles;
+    public void setMainEst(List<String> mainEst) {
+        this.mainEst = mainEst;
     }
 
-    public List<String> getOuestPiques() {
-        return ouestPiques;
-    }
-
-    public void setOuestPiques(List<String> ouestPiques) {
-        this.ouestPiques = ouestPiques;
-    }
-
-    public List<String> getOuestCoeurs() {
-        return ouestCoeurs;
-    }
-
-    public void setOuestCoeurs(List<String> ouestCoeurs) {
-        this.ouestCoeurs = ouestCoeurs;
-    }
-
-    public List<String> getOuestCarreaux() {
-        return ouestCarreaux;
-    }
-
-    public void setOuestCarreaux(List<String> ouestCarreaux) {
-        this.ouestCarreaux = ouestCarreaux;
-    }
-
-    public List<String> getOuestTrefles() {
-        return ouestTrefles;
-    }
-
-    public void setOuestTrefles(List<String> ouestTrefles) {
-        this.ouestTrefles = ouestTrefles;
-    }
-
-    public List<String> getNordPiques() {
-        return nordPiques;
-    }
-
-    public void setNordPiques(List<String> nordPiques) {
-        this.nordPiques = nordPiques;
-    }
-
-    public List<String> getNordCoeurs() {
-        return nordCoeurs;
-    }
-
-    public void setNordCoeurs(List<String> nordCoeurs) {
-        this.nordCoeurs = nordCoeurs;
-    }
-
-    public List<String> getNordCarreaux() {
-        return nordCarreaux;
-    }
-
-    public void setNordCarreaux(List<String> nordCarreaux) {
-        this.nordCarreaux = nordCarreaux;
-    }
-
-    public List<String> getNordTrefles() {
-        return nordTrefles;
-    }
-
-    public void setNordTrefles(List<String> nordTrefles) {
-        this.nordTrefles = nordTrefles;
-    }
-
-    public List<String> getEstPiques() {
-        return estPiques;
-    }
-
-    public void setEstPiques(List<String> estPiques) {
-        this.estPiques = estPiques;
-    }
-
-    public List<String> getEstCoeurs() {
-        return estCoeurs;
-    }
-
-    public void setEstCoeurs(List<String> estCoeurs) {
-        this.estCoeurs = estCoeurs;
-    }
-
-    public List<String> getEstCarreaux() {
-        return estCarreaux;
-    }
-
-    public void setEstCarreaux(List<String> estCarreaux) {
-        this.estCarreaux = estCarreaux;
-    }
-
-    public List<String> getEstTrefles() {
-        return estTrefles;
-    }
-
-    public void setEstTrefles(List<String> estTrefles) {
-        this.estTrefles = estTrefles;
+    public void sortMainsPlanchette() {
+        Collections.sort(mainSud);
+        Collections.sort(mainOuest);
+        Collections.sort(mainNord);
+        Collections.sort(mainEst);
     }
 
     @Override
     public String toString() {
-        return "PlanchetteBridge [sudPiques=" + sudPiques + ", sudCoeurs=" + sudCoeurs + ", sudCarreaux=" + sudCarreaux + ", sudTrefles=" + sudTrefles
-                + ", ouestPiques=" + ouestPiques + ", ouestCoeurs=" + ouestCoeurs + ", ouestCarreaux=" + ouestCarreaux + ", ouestTrefles=" + ouestTrefles
-                + ", nordPiques=" + nordPiques + ", nordCoeurs=" + nordCoeurs + ", nordCarreaux=" + nordCarreaux + ", nordTrefles=" + nordTrefles
-                + ", estPiques=" + estPiques + ", estCoeurs=" + estCoeurs + ", estCarreaux=" + estCarreaux + ", estTrefles=" + estTrefles + "]";
+        return "PlanchetteBridge [mainSud=" + mainSud + ", mainOuest=" + mainOuest + ", mainNord=" + mainNord + ", mainEst=" + mainEst + "]";
+    }
+
+    public static void main(String[] args) {
+        PlanchetteBridge p = new PlanchetteBridge();
+        List<String> s = p.getMainSud();
+        List<String> o = p.getMainOuest();
+        List<String> n = p.getMainNord();
+        List<String> e = p.getMainEst();
+
+        s.add("3");
+        s.add("2");
+        s.add("1");
+        o.add("3");
+        o.add("2");
+        o.add("1");
+        n.add("3");
+        n.add("2");
+        n.add("1");
+        e.add("3");
+        e.add("2");
+        e.add("1");
+
+        System.out.println(p);
+        p.sortMainsPlanchette();
+        System.out.println(p);
+
     }
 
 }
